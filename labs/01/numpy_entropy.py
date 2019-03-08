b0c176efabd8606a0a2d180cc7fa4f6aeb32be82
kopalja <kopal.jakub@gmail.com>
2019-03-08 22:01:33 +0100

    du1-a

--- labs/01/numpy_entropy.py
+++ labs/01/numpy_entropy.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
+# dd7e3410-38c0-11e8-9b58-00505601122b
+# 6e14ef6b-3281-11e8-9de3-00505601122b
+
 import numpy as np
 
 
 def dict_to_array(d):
-    last_key = ord(max(d.keys())) - ord('A')
-    array, number = np.zeros(last_key + 1), sum(d.values())
-    for key, value in d.items():
-        array[ord(key) - ord('A')] = value / number
+    array = np.zeros(len(d))
+    number = sum(d.values())
+    i = 0
+    for key, value in sorted(d.items()):
+        array[i] = value / number
+        i += 1
     return array
+
+def add_keys(source, dest):
+    for key in source:
+        if not key in dest:
+            dest[key] = 0      
 
 if __name__ == "__main__":
     # Load data distribution, each data point on a line
@@ -25,7 +35,7 @@ if __name__ == "__main__":
     # TODO: Create a NumPy array containing the data distribution. The
     # NumPy array should contain only data, not any mapping. If required,
     # the NumPy array might be created after loading the model distribution.
-    data_array = dict_to_array(data_dict)
+    #data_array = dict_to_array(data_dict)
 
 
     # Load model distribution, each line `word \t probability`.
@@ -36,9 +46,11 @@ if __name__ == "__main__":
             # TODO: process the line, aggregating using Python data structures
             key, value = line.split('\t')
             model_dict[key] = float(value)
+    
+
 
     # TODO: Create a NumPy array containing the model distribution.
-    model_array = dict_to_array(model_dict)
+    data_array = dict_to_array(data_dict)
 
     # TODO: Compute and print the entropy H(data distribution). You should not use
     # manual for/while cycles, but instead use the fact that most NumPy methods
@@ -46,19 +58,14 @@ if __name__ == "__main__":
     entropy = - np.sum(data_array * np.log(data_array))
     print("{:.2f}".format(entropy))
 
+    add_keys(model_dict, data_dict)
+    add_keys(data_dict, model_dict)
+    data_array = dict_to_array(data_dict)
+    model_array = dict_to_array(model_dict)
+
     # TODO: Compute and print cross-entropy H(data distribution, model distribution)
     # and KL-divergence D_KL(data distribution, model_distribution)
-    data_size = data_array.shape[0]
-    model_size = model_array.shape[0]
-    if data_size < model_size:
-        data_array = np.append(data_array, np.zeros(model_size - data_size))
-    elif data_size > model_size:
-        model_array = np.append(model_array, np.zeros(data_size - model_size))
-
-
-
     cross_entropy = - np.sum(data_array * np.log(model_array))
     print("{:.2f}".format(cross_entropy))
-
     kl_divergence = cross_entropy - entropy
     print("{:.2f}".format(kl_divergence))
